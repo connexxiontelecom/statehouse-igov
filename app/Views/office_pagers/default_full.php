cd24c931bd062ddd826ca5e24d9360e4388c14fa
peterejiro <peterejiro96@gmail.com>
2021-06-26 07:37:40 +0100

    pagination on notice board completed

--- app/Views/office_pagers/default_full.php
+++ app/Views/office_pagers/default_full.php
@@ -3,6 +3,13 @@
 <div class="row">
 	<div class="col-4 offset-3">
 		<ul class="pagination pagination-rounded justify-content-end mb-3">
+			
+			<li class="page-item">
+				<a class="page-link" href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
+					<span aria-hidden="true">«</span>
+					<span class="sr-only"><?= lang('Pager.first') ?></span>
+				</a>
+			</li>
 			<?php if ($pager->hasPrevious()) : ?>
 			<li class="page-item">
 				<a class="page-link" href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
@@ -32,19 +39,20 @@
 			<?php if ($pager->hasNext()) : ?>
 				<li class="page-item">
 					<a class="page-link" href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
-						<span aria-hidden="true">«</span>
+						<span aria-hidden="true">»</span>
 						<span class="sr-only"><?= lang('Pager.next') ?></span>
 					</a>
 				</li>
 				
-				<li class="page-item">
-					<a class="page-link" href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>">
-						<span aria-hidden="true">«</span>
-						<span class="sr-only"><?= lang('Pager.last') ?></span>
-					</a>
-				</li>
+				
 			
 			<?php endif; ?>
+			<li class="page-item">
+				<a class="page-link" href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>">
+					<span aria-hidden="true">»</span>
+					<span class="sr-only"><?= lang('Pager.last') ?></span>
+				</a>
+			</li>
 		</ul>
 	</div> <!-- end col-->
 </div>
