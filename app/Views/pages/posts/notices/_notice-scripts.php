f977a1104f856d67ea1bcacbd8bfb399e12c1b5f
CJ Amanambu <oamanambu@yahoo.com>
2021-07-19 14:19:11 +0100

    refactores create new notice

--- app/Views/pages/posts/notices/_notice-scripts.php
+++ app/Views/pages/posts/notices/_notice-scripts.php
@@ -1,45 +1,6 @@
+<?=view('pages/posts/_post-scripts.php')?>
 <script>
 	$(document).ready(function () {
-		$('form#new-notice-form').submit(function (e) {
-			e.preventDefault()
-      let subject = $('#subject').val()
-      let signedBy = $('#signed-by').val()
-      if (!subject || !signedBy || quillEditor.root.innerText.length < 2) {
-        Swal.fire('Invalid Submission!', 'Please fill in all required fields', 'error')
-      } else {
-        let body = quillEditor.root.innerHTML
-        let formData = new FormData(this)
-        formData.set('body', body)
-        Swal.fire({
-          title: 'Are you sure?',
-          text: 'This will submit your notice to the iGov system',
-          type: 'warning',
-          showCancelButton: true,
-          confirmButtonText: 'Confirm',
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33"
-        }).then(confirm => {
-          if (confirm.value) {
-            $.ajax({
-              url: '<?=site_url('/new-notice')?>',
-              type: 'post',
-              data: formData,
-              success: response => {
-                if (response.success) {
-                  Swal.fire('Confirmed!', response.message, 'success').then(() => location.href = '<?=site_url('/my-notices')?>')
-                } else {
-                  Swal.fire('Sorry!', response.message, 'error')
-                }
-              },
-              cache: false,
-              contentType: false,
-              processData: false
-            })
-          }
-        })
-      }
-    })
-
     $('form#edit-notice-form').submit(function (e) {
       e.preventDefault()
       let body = quillEditor.root.innerHTML
@@ -74,4 +35,44 @@
       })
     })
   })
+
+  function createNotice() {
+	  let noticeForm = $('form#new-notice-form')[0]
+    let refNo = $('#ref-no').val()
+    let subject = $('#subject').val()
+    let signedBy = $('#signed-by').val()
+    if (!refNo || !subject || !signedBy || quillEditor.root.innerText.length < 2) {
+      Swal.fire('Invalid Submission!', 'Please fill in all required fields', 'error')
+    } else {
+      let formData = new FormData(noticeForm)
+      formData.append('p_body', quillEditor.root.innerHTML)
+      Swal.fire({
+        title: 'Are you sure?',
+        text: 'This will submit your notice to the iGov system',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Confirm',
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33"
+      }).then(confirm => {
+        if (confirm.value) {
+          $.ajax({
+            url: '<?=site_url('/new-notice')?>',
+            type: 'post',
+            data: formData,
+            success: response => {
+              if (response.success) {
+                Swal.fire('Confirmed!', response.message, 'success').then(() => location.href = '<?=site_url('/my-notices')?>')
+              } else {
+                Swal.fire('Sorry!', response.message, 'error')
+              }
+            },
+            cache: false,
+            contentType: false,
+            processData: false
+          })
+        }
+      })
+    }
+  }
 </script>
