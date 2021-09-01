1e9ce798b79c16aab7284b12fa3a278bccb6c19e
CJ Amanambu <oamanambu@yahoo.com>
2021-09-01 16:54:20 +0100

    adds hotfixes to auth pages

--- app/Views/auth/access_denied.php
+++ app/Views/auth/access_denied.php
@@ -8,7 +8,7 @@
 	<meta content="Coderthemes" name="author" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<!-- App favicon -->
-	<link rel="shortcut icon" href="/assets/images/favicon.ico">
+	<link rel="shortcut icon" href="/assets/images/logo-sm.png">
 	
 	<!-- App css -->
 	<link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
