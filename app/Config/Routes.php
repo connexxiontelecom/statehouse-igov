4640fb570e1e057d06f63c6f8b0f473203e39143
peterejiro <peterejiro96@gmail.com>
2021-09-10 10:31:46 +0100

    updated route for renewals schedules

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -269,6 +269,7 @@ $routes->match(['get', 'post'], 'new-vehicle', 'FleetController::new_vehicle', [
 $routes->match(['get'], 'drivers', 'FleetController::drivers', ['filter' => 'auth']);
 $routes->match(['get', 'post'], 'new-driver', 'FleetController::new_driver', ['filter' => 'auth']);
 $routes->match(['get', 'post'], 'manage-vehicle/(:num)', 'FleetController::manage_vehicle/$1', ['filter' => 'auth']);
+$routes->match(['get', 'post'], 'renewal-schedules', 'FleetController::renewal_schedules', ['filter' => 'auth']);
 
 
 /*
